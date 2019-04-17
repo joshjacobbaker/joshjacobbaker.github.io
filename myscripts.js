9253ca0b39a23a87382ee9afe72cf8b660f0abf8
Joshua Baker <33912225+joshjacobbaker@users.noreply.github.com>
2019-04-16 19:33:10 -0700

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -4,10 +4,20 @@ var images = [];	// Images Array
 var time = 3000;	// Time Between Switch
 	 
 // Image List
-images[0] = "Tofino.jpg";
-images[1] = "APICS_SOY_award.PNG";
-images[2] = "DSCI_profs.JPG";
-images[3] = "Boeing_Example_FloorPlan.PNG";
+images[0] = "Nylatech_Before_Forklift.PNG";
+images[1] = "Nylatech_Project_Step_1.PNG";
+images[2] = "Nylatech_Project_Step_2.PNG";
+images[3] = "Nylatech_Project_Step_3.PNG";
+images[4] = "Nylatech_Project_Step_4.PNG";
+images[5] = "Nylatech_Project_Step_5.PNG";
+images[6] = "Nylatech_Project_Step_6.PNG";
+images[7] = "Nylatech_Project_Step_7.PNG";
+images[8] = "Nylatech_Project_Step_8.PNG";
+images[9] = "Nylatech_Project_Step_9.PNG";
+images[10] = "Nylatech_Project_Step_10.PNG";
+images[11] = "Nylatech_Project_Step_11.PNG";
+images[12] = "Nylatech_Project_Step_12.PNG";
+images[13] = "Nylatech_After_Forklift.PNG";
 
 // Change Image
 function changeImg(){
