6fdf2f16755ac08f6308dae854a6ca1610052868
Joshua Baker <33912225+joshjacobbaker@users.noreply.github.com>
2019-04-16 22:54:22 -0700

    Update myScripts2.js

--- myScripts2.js
+++ myScripts2.js
@@ -4,19 +4,9 @@ var imagesj = [];	// Images Array
 var timej = 2000;	// Time Between Switch
 	 
 // Image List
-imagesj[0] = "Nylatech_Before_Forklift.PNG";
-imagesj[1] = "Nylatech_Project_Step_1.PNG";
-imagesj[2] = "Nylatech_Project_Step_2.PNG";
-imagesj[3] = "Nylatech_Project_Step_3.PNG";
-imagesj[4] = "Nylatech_Project_Step_4.PNG";
-imagesj[5] = "Nylatech_Project_Step_5.PNG";
-imagesj[6] = "Nylatech_Project_Step_6.PNG";
-imagesj[7] = "Nylatech_Project_Step_7.PNG";
-imagesj[8] = "Nylatech_Project_Step_8.PNG";
-imagesj[9] = "Nylatech_Project_Step_9.PNG";
-imagesj[10] = "Nylatech_Project_Step_11.PNG";
-imagesj[11] = "Nylatech_Project_Step_12.PNG";
-imagesj[12] = "Nylatech_After_Forklift.PNG";
+imagesj[0] = "Tofino.jpg";
+imagesj[1] = "WWU_best_ski_school.PNG";
+
 
 
 // Change Image
