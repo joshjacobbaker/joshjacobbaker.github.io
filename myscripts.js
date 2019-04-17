255ffd83378eda8c939ce3c5303105d2020622ad
Joshua Baker <33912225+joshjacobbaker@users.noreply.github.com>
2019-04-16 20:45:03 -0700

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -39,3 +39,46 @@ function changeImg(){
 // Run function when page loads
 window.onload=changeImg;
 
+// Second 
+
+
+
+var j = 0; 			// Start Point
+var images = [];	// Images Array
+var time = 2000;	// Time Between Switch
+	 
+// Image List
+images[0] = "Nylatech_Before_Forklift.PNG";
+images[1] = "Nylatech_Project_Step_1.PNG";
+images[2] = "Nylatech_Project_Step_2.PNG";
+images[3] = "Nylatech_Project_Step_3.PNG";
+images[4] = "Nylatech_Project_Step_4.PNG";
+images[5] = "Nylatech_Project_Step_5.PNG";
+images[6] = "Nylatech_Project_Step_6.PNG";
+images[7] = "Nylatech_Project_Step_7.PNG";
+images[8] = "Nylatech_Project_Step_8.PNG";
+images[9] = "Nylatech_Project_Step_9.PNG";
+images[10] = "Nylatech_Project_Step_11.PNG";
+images[11] = "Nylatech_Project_Step_12.PNG";
+images[12] = "Nylatech_After_Forklift.PNG";
+
+
+// Change Image
+function changejImg(){
+	document.slide.src = images[j];
+
+	// Check If Index Is Under Max
+	if(i < images.length - 1){
+	  // Add 1 to Index
+	  j++; 
+	} else { 
+		// Reset Back To O
+		j = 0;
+	}
+
+	// Run function every x seconds
+	setTimeout("changejImg()", time);
+}
+
+// Run function when page loads
+window.onload=changeImg;
